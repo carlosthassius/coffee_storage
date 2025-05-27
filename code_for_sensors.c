bc50fb03d26582066d246658a18be4c266f19a1e
db075239 <154441156+db075239@users.noreply.github.com>
2025-05-27 15:46:51 +0200

    Update code_for_sensors.c
    when sensors fail, we get default values

--- code_for_sensors.c
+++ code_for_sensors.c
@@ -2,6 +2,7 @@
 #include "dev/leds.h"
 #include "dev/dht22.h"
 #include "dev/adc-sensors.h"
+#include "dev/button-sensor.h"
 #include <stdio.h>
 
 /* --- Configuration --- */
@@ -20,44 +21,64 @@ PROCESS_THREAD(smart_coffee_gadget_process, ev, data)
 {
   PROCESS_BEGIN();
 
-  int temperature, humidity;
-  uint16_t rotation;
+  int temperature = 24; // Default 24.5°C
+  int humidity = 650;    // Default 65.0%
+  uint16_t rotation = 150; // Default rotation value
 
   /* Activate sensors */
   SENSORS_ACTIVATE(dht22);
-  adc_sensors.configure(ANALOG_PHIDGET_ROTATION_1109, ROTARY_SENSOR_PIN);
+  adc_zoul.configure(ANALOG_PHIDGET_ROTATION_1109, ROTARY_SENSOR_PIN);
+  SENSORS_ACTIVATE(button_sensor);
 
   while(1) {
     etimer_set(&sensor_timer, SENSOR_READ_INTERVAL);
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&sensor_timer));
+    PROCESS_WAIT_EVENT();
 
     /* --- Read DHT22 Sensor --- */
-    if(dht22_read_all(&temperature, &humidity) != DHT22_ERROR) {
-      printf("Temperature: %d.%d °C, Humidity: %d.%d %%RH\n",
-             temperature / 10, temperature % 10,
-             humidity / 10, humidity % 10);
-    } else {
-      printf("DHT22 read failed!\n");
+    if(ev == PROCESS_EVENT_TIMER && etimer_expired(&sensor_timer)) {
+      if(dht22_read_all(&temperature, &humidity) == DHT22_ERROR) {
+        temperature=245;
+        humidity=650;
+        printf("[Mock] Temperature: %d.%d °C, Humidity: %d.%d %%RH\n",
+               temperature / 10, temperature % 10,
+               humidity / 10, humidity % 10);
+      } else {
+        printf("Temperature: %d.%d °C, Humidity: %d.%d %%RH\n",
+               temperature / 10, temperature % 10,
+               humidity / 10, humidity % 10);
+      }
+
+      /* Small delay to avoid conflicts */
+      clock_delay_usec(1000); // 100 ms
+
+      /* --- Read Rotary Sensor --- */
+      rotation = adc_zoul.value(ANALOG_PHIDGET_ROTATION_1109);
+      if(rotation == 0 || rotation == ADC_WRAPPER_ERROR) {
+        rotation=150;
+        printf("[Mock] Rotary angle raw value: %u\n", rotation);
+      } else {
+        printf("Rotary angle raw value: %u\n", rotation);
+      }
+
+      /* Optional LED Feedback */
+      if(rotation < 100) {
+        leds_on(LEDS_GREEN);
+        leds_off(LEDS_RED | LEDS_BLUE);
+      } else if(rotation < 300) {
+        leds_on(LEDS_BLUE);
+        leds_off(LEDS_GREEN | LEDS_RED);
+      } else {
+        leds_on(LEDS_RED);
+        leds_off(LEDS_GREEN | LEDS_BLUE);
+      }
     }
 
-    /* --- Read Rotary Sensor --- */
-    rotation = adc_sensors.value(ANALOG_PHIDGET_ROTATION_1109);
-    if(rotation != ADC_WRAPPER_ERROR) {
-      printf("Rotary angle raw value: %u\n", rotation);
-    } else {
-      printf("Failed to read rotary sensor. Check wiring.\n");
-    }
-
-    /* Optional LED Feedback */
-    if(rotation < 100) {
-      leds_on(LEDS_GREEN);
-      leds_off(LEDS_RED | LEDS_BLUE);
-    } else if(rotation < 300) {
-      leds_on(LEDS_BLUE);
-      leds_off(LEDS_GREEN | LEDS_RED);
-    } else {
+    /* --- Handle Button Press --- */
+    if(ev == sensors_event && data == &button_sensor) {
+      printf("[NOTIFICATION] Coffee storage is LOW! Please refill.\n");
       leds_on(LEDS_RED);
-      leds_off(LEDS_GREEN | LEDS_BLUE);
+      clock_delay_usec(1000); // Blink effect
+      leds_off(LEDS_RED);
     }
   }
 
